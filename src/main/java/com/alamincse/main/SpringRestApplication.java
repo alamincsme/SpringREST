b8e8e7a2208c3e00b10c032ed717e2cf3f54172f
alamincsme <alamincsme@gmail.com>
2024-01-08 15:28:54 +0600

    learning Spring REST

---
+++ src/main/java/com/alamincse/main/SpringRestApplication.java
@@ -0,0 +1,16 @@
+package com.alamincse.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApplication.class, args);
+		System.out.println("alamin");
+		System.out.print("You");
+		System.out.println("hello ");
+	}
+
+}
